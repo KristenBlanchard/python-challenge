01d5a9fa9bfb330e412cf4c3df89197e371b6bcb
Kristen Blanchard <KristenBlanchard@Kristens-MacBook-Pro.local>
2020-08-09 18:04:17 -0400

    PyBank Update

--- PyBank/main.py
+++ PyBank/main.py
@@ -47,3 +47,12 @@ with open(csvpath,'r') as csvfile:
     x = revenue_change.index(revenue_increase)
     month_increase = month[x+1]
     print(month_increase)
+
+    # greatest decrease value
+    revenue_decrease = min(revenue_change)
+    print("$" + str(revenue_decrease))
+
+    # greatest decrease month
+    z = revenue_change.index(revenue_decrease)
+    month_decrease = month[z+1]
+    print(month_decrease)
